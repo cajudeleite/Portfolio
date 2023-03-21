567c813b13b67821681befb45cabd397e6b8706e
Nathan Devooght <nathan@spellz.app>
2023-03-21 13:17:50 +0100

    Colors and dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useLayoutEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  const rootDiv = document.getElementById("root") as HTMLDivElement;
+
+  useLayoutEffect(() => {
+    const preferedTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const localTheme = "theme" in localStorage && localStorage.theme === "dark";
+    const darkTheme = localTheme || preferedTheme;
+
+    setDarkMode(darkTheme);
+    if (darkTheme) rootDiv.classList.add("dark");
+  }, []);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    localStorage.setItem("theme", !darkMode ? "dark" : "light");
+    rootDiv.classList.toggle("dark");
+  };
 
   return (
-    <div className="App">
+    <div className="">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,18 +33,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={toggleDarkMode}>Toggle dark mode</button>
+        <p>Mode: {darkMode ? "dark" : "light"}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
