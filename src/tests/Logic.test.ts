7acf3d93021fce791939f7ddabb2ca9ddfe1a2d6
Nathan Debaz <nathandebaz266@gmail.com>
2023-03-28 01:08:49 +0200

    srLatch

--- src/tests/Logic.test.ts
+++ src/tests/Logic.test.ts
@@ -1,4 +1,4 @@
-import { and, not, nand, or, xor } from "../utils/Logic";
+import { and, not, nand, or, xor, nor, srLatch, dLatch } from "../utils/Logic";
 
 describe("\nTesting AND", () => {
   test("a = false, b = false", () => expect(and(false, false)).toBe(false));
@@ -31,4 +31,64 @@ describe("\nTesting XOR", () => {
   test("a = false, b = true", () => expect(xor(false, true)).toBe(true));
   test("a = true, b = false", () => expect(xor(true, false)).toBe(true));
   test("a = true, b = true", () => expect(xor(true, true)).toBe(false));
+});
+
+describe("\nTesting NOR", () => {
+  test("a = false, b = false", () => expect(nor(false, false)).toBe(true));
+  test("a = false, b = true", () => expect(nor(false, true)).toBe(false));
+  test("a = true, b = false", () => expect(nor(true, false)).toBe(false));
+  test("a = true, b = true", () => expect(nor(true, true)).toBe(false));
+});
+
+const testLatch = srLatch();
+
+describe("\nTesting srLatch", () => {
+  test("Should initialize to false", () => {
+    expect(testLatch(false, false)).toBe(false);
+  });
+
+  test("Should set to true when set is true", () => {
+    expect(testLatch(true, false)).toBe(true);
+  });
+
+  test("Should reset to false when reset is true", () => {
+    expect(testLatch(false, true)).toBe(false);
+  });
+
+  test("Should maintain state", () => {
+    expect(testLatch(true, false)).toBe(true);
+    expect(testLatch(false, false)).toBe(true);
+    expect(testLatch(false, true)).toBe(false);
+    expect(testLatch(false, false)).toBe(false);
+  });
+});
+
+describe("\nTesting dLatch", () => {
+  test("Should initialize to false", () => {
+    expect(dLatch(false, false, testLatch)).toBe(false);
+  });
+
+  test("Shouldn't store if only data is set", () => {
+    expect(dLatch(true, false, testLatch)).toBe(false);
+    expect(dLatch(false, false, testLatch)).toBe(false);
+  });
+
+  test("Should store if both data and store are set", () => {
+    expect(dLatch(true, true, testLatch)).toBe(true);
+    expect(dLatch(true, false, testLatch)).toBe(true);
+    expect(dLatch(false, false, testLatch)).toBe(true);
+  });
+
+  test("Should keep the stored value if data is toggled", () => {
+    expect(dLatch(true, false, testLatch)).toBe(true);
+    expect(dLatch(false, false, testLatch)).toBe(true);
+    expect(dLatch(false, true, testLatch)).toBe(false);
+    expect(dLatch(true, false, testLatch)).toBe(false);
+  });
+
+  test("Should reset if stored is set", () => {
+    expect(dLatch(true, true, testLatch)).toBe(true);
+    expect(dLatch(false, true, testLatch)).toBe(false);
+    expect(dLatch(false, false, testLatch)).toBe(false);
+  });
 });
--- src/utils/Logic.ts
+++ src/utils/Logic.ts
@@ -3,3 +3,19 @@ export const not = (a: boolean) => !a;
 export const nand = (a: boolean, b: boolean) => not(and(a, b));
 export const or = (a: boolean, b: boolean) => nand(not(a), not(b));
 export const xor = (a: boolean, b: boolean) => and(nand(a, b), or(a, b));
+export const nor = (a: boolean, b: boolean) => not(or(a, b));
+
+export const srLatch = () => {
+  let state = false;
+
+  return (set: boolean, reset: boolean) => {
+    state = nor(reset, nor(set, state));
+    return state;
+  };
+};
+
+export const dLatch = (
+  data: boolean,
+  store: boolean,
+  latch: (set: boolean, reset: boolean) => boolean
+) => latch(and(data, store), and(store, not(data)));
