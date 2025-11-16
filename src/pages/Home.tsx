ec9209e957fa0d7f880b2c2c60e09a41f99a545f
cajudeleite <nathandebaz266@gmail.com>
2025-11-16 19:38:45 -0300

    Home page redone

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
+import BlurText from "../components/bits/BlurText";
+import SplitText from "../components/bits/SplitText";
 import { title } from "../utils/classNameLibrary";
+import useDarkMode from "../stores/darkModeStore";
 
 const Home = () => {
+  const [showCaju, setShowCaju] = useState(false);
+  const { darkMode } = useDarkMode();
+
   return (
     <>
-      <h2 className="text-xl md:text-2xl">
-        Hello, my name is{" "}
-        <span className="text-primary text-3xl font-medium">Nate</span>, but
-        you can call me
-      </h2>
-      <h1 className={`text-secondary ${title}`}>Caju</h1>
+      <BlurText
+        text={<>Hello, my name is <span className="text-primary text-3xl font-medium">Nate</span>, but you can call me</>}
+        delay={35}
+        animateBy="letters"
+        direction="top"
+        className="text-xl md:text-2xl"
+        onAnimationComplete={() => setShowCaju(true)}
+      />
+      {showCaju ? (
+        <SplitText
+          text="Caju"
+          className={`text-secondary ${title}`}
+          delay={150}
+          duration={1}
+          splitType="chars"
+          textAlign="left"
+          tag="h1"
+        />
+      ) : <h1 className={`${darkMode ? "text-dark" : "text-light"} ${title} `}>Caju</h1>}
     </>
   );
 };
 
-export default Home;
+export default Home;  
