87c4e5487763008e00a0123397cd9f63a99ad5a2
cajudeleite <nathandebaz266@gmail.com>
2025-12-03 14:18:34 -0300

    Skills done

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,24 +1,24 @@
 import { title } from "../utils/classNameLibrary";
-import SkillBlocks from "../components/SkillBlocks";
 import MagicBento from "../components/bits/MagicBento";
+import useFpsThreshold from "../stores/fpsThresholdStore";
 
-const Skills = () => (
-  <>
+const Skills = () => {
+  const { fpsThreshold } = useFpsThreshold();
+  
+  return <>
     <h1 className={`text-secondary ${title}`}>Skills</h1>
-    {/* <SkillBlocks /> */}
     <MagicBento 
       textAutoHide={true}
       enableStars={true}
       enableSpotlight={false}
       enableBorderGlow={true}
       enableTilt={false}
-      enableMagnetism={true}
+      enableMagnetism={false}
       clickEffect={true}
-      spotlightRadius={300}
-      particleCount={12}
+      particleCount={fpsThreshold / 2}
       glowColor="202, 94, 4"
     />
   </>
-);
+};
 
 export default Skills;
